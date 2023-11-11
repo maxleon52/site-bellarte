4ecd835f7f73d6d17dbd0203a57bcccb0abb5409
maxleon52 <maxleon522@gmail.com>
2023-11-10 22:10:03 -0300

    task: create page show one "produto"

---
+++ src/app/(produtos)/SlideProduct.tsx
@@ -0,0 +1,112 @@
+/* eslint-disable jsx-a11y/alt-text */
+"use client";
+
+import React, { useState } from "react";
+
+// import required modules
+import type { Swiper as SwiperProps } from "swiper";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+export default function SlideProduct() {
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperProps | null>(null);
+
+  return (
+    <>
+      <Swiper
+        // style={{
+        //   "--swiper-navigation-color": "#fff",
+        //   "--swiper-pagination-color": "#fff",
+        // }}
+        loop={true}
+        spaceBetween={10}
+        navigation={true}
+        // thumbs={{ swiper: thumbsSwiper }}
+        thumbs={{
+          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+        }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper2 flex-none !border-2 border-red-400 xl:!h-[60vh]"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+        </SwiperSlide>
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        navigation={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
--- src/app/(produtos)/produtos/[id]/page.tsx
+++ src/app/(produtos)/produtos/[id]/page.tsx
@@ -1,3 +1,89 @@
+import { Facebook, Instagram, Share2, ShoppingCart } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import SlideProduct from "../../SlideProduct";
+
 export default function Product({ params }: { params: { id: string } }) {
-  return <div>Meu Produto: {params.id}</div>;
+  return (
+    <section className="cont grid flex-1 gap-6 px-4 py-8 md:grid-cols-2 md:px-8 xl:px-16 2xl:px-40">
+      <div className="max-w-full overflow-hidden">
+        <SlideProduct />
+      </div>
+
+      <div className="grid flex-none gap-4">
+        <h2 className="text-3xl font-semibold text-transparent text-zinc-500 md:text-5xl">
+          Nome do item
+        </h2>
+
+        <div className="flex items-center justify-between">
+          <span className="text-3xl text-zinc-400">R$ 999,00</span>
+          <Select defaultValue="1">
+            <SelectTrigger className="border-zinc-300 text-zinc-400 !ring-0 !ring-offset-0 md:max-w-[160px] md:text-lg">
+              <SelectValue placeholder="Selecione uma categoria" />
+            </SelectTrigger>
+            <SelectContent className="">
+              {[1, 2, 3, 4, 5, 6].map((item) => (
+                <SelectItem
+                  key={item}
+                  value={String(item)}
+                  className="text-zinc-400"
+                >
+                  {item}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+
+        <Button className="h-12 w-full">COMPRAR</Button>
+        <Button
+          className="h-12 w-full border-zinc-400 text-zinc-500"
+          variant="outline"
+        >
+          ADICIONAR AO CARRINHO <ShoppingCart className="ml-2 h-4" />
+        </Button>
+
+        <div className="flex flex-col gap-4">
+          <span className="text-xl font-semibold text-zinc-500 ">
+            Compartilhar
+          </span>
+
+          <div className="flex gap-4">
+            <Facebook className="cursor-pointer stroke-zinc-400 transition-all hover:opacity-75" />
+            <Instagram className="cursor-pointer stroke-zinc-400 transition-all hover:opacity-75" />
+            <Share2 className="cursor-pointer stroke-zinc-400 transition-all hover:opacity-75" />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <span className="text-xl font-semibold text-zinc-500">Descrição</span>
+
+          <p className="text-xl text-zinc-400">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione ad
+            totam, laboriosam magnam quaerat repellat corporis ab natus
+            necessitatibus. Expedita minus, possimus magnam accusantium
+            blanditiis iusto hic tempora ex nisi. Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Ratione ad totam, laboriosam magnam
+            quaerat repellat corporis ab natus necessitatibus. Expedita minus,
+            possimus magnam accusantium blanditiis iusto hic tempora ex nisi.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione ad
+            totam, laboriosam magnam quaerat repellat corporis ab natus
+            necessitatibus. Expedita minus, possimus magnam accusantium
+            blanditiis iusto hic tempora ex nisi. Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Ratione ad totam, laboriosam magnam
+            quaerat repellat corporis ab natus necessitatibus. Expedita minus,
+            possimus magnam accusantium blanditiis iusto hic tempora ex nisi.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
