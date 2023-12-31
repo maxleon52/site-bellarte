982283d2e364f45faff71097000baa0d85dcb6a8
maxleon52 <maxleon522@gmail.com>
2023-12-31 13:45:01 -0300

    fix: sitemap generate

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,14 +4,16 @@ import { client } from "@/lib/sanity";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const query = `*[_type == 'product']{
+    _id,
     "slug":slug.current,
+    category->{name, "slug": slug.current},
   }`;
 
   const response = await client.fetch(query);
 
   return [
     ...response.map((item: any) => ({
-      url: `https://www.bellartecroche.com.br/${item.slug}`,
+      url: `https://www.bellartecroche.com.br/produtos/${item.category.name}/${item._id}`,
       lastModified: new Date(),
     })),
   ];
